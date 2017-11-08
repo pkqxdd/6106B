42cf24352851044cedad427da6d83768ff18d34e
Ruocheng Wang <pkqxdd@users.noreply.github.com>
2017-11-08 09:12:09 +0800

    Bugs Fix

--- Driver.h
+++ Driver.h
@@ -3,15 +3,19 @@
 
 // -----------------BEGIN Global Variables Configuration-------------------
 
-const int MB_POWER = 80; // power to mobile goal lift
-const int FOURBAR_POWER = 64; //power to fourbar
-const int FOURBAR_ANTIGRAVITY = 20; //power to fourbar when it is in the "stop" position
-const int MB_ANTIGRAVITY=5; //power to mobile goal lift when it is in the "stop" position
+const int MB_POWER = 90; // power to mobile goal lift
+const int FOURBAR_POWER = 100; //power to fourbar
+const int CHAINBAR_POWER=80; // power to chainbar
+const int ROLLER_POWER=80; // power to chainbar
+const int FOURBAR_ANTIGRAVITY = 0; //power to fourbar when it is in the "stop" position
+const int MB_ANTIGRAVITY=0; //power to mobile goal lift when it is in the "stop" position
+const int CHAINBAR_ANTIGRAVITY=20;
+const int ROLLER_ANTIGRAVITY=30;
 
 // ------------------END Global Variables Configuration----------------------
 // -----------------BEGIN Keymap Configuration------------------
 
-#include "Keymap.h"
+#include "KeymapXavier.h" // this file defines which button is bind to which key
 
 // -----------------END Keymap Configuration----------------
 // ------------------BEGIN Utility Functons---------------
@@ -27,6 +31,70 @@ void moveRightWheels(int power)
 	motor[front_right] = power;
 	motor[back_right] = power;
 }
+
+short wheelRecordLeft[100];
+short wheelRecordRight[100];
+
+short index=0;
+short getRecordIndex(){
+	if (index == 99)
+	{
+		index=0;
+		return index;
+	}
+	else {
+	return index++;
+}
+}
+
+short getAverageWheelHistoryLeft(){
+	int sum=0;
+	for (short i=index;i>=0;i--)
+	{
+	sum+=wheelRecordLeft[i];
+}
+return sum/index;
+
+}
+
+
+short getAverageWheelHistoryRight(){
+	int sum=0;
+	for (short i=index;i>=0;i--)
+	{
+	sum+=wheelRecordRight[i];
+}
+return sum/index;
+
+}
+
+void bufferedWheelsControls(int powerLeft, int powerRight){
+short tmpIndex=getRecordIndex();
+if (powerLeft <= 0 || powerRight <=0 ){
+	wheelRecordLeft[tmpIndex]=powerLeft;
+	wheelRecordRight[tmpIndex]=powerRight;
+	moveLeftWheels(powerLeft);
+	moveRightWheels(powerRight);
+	return;
+} else {
+short history[2]={getAverageWheelHistoryLeft(),getAverageWheelHistoryRight()};
+if (history[0] <= -20 && history[1] <=-20){ // decrease the power to wheels overtime in this case
+	wheelRecordLeft[tmpIndex]=powerLeft/log10(abs(history[0]));
+	wheelRecordRight[tmpIndex]=powerRight/log10(abs(history[1]));
+	moveLeftWheels(powerLeft/log10(abs(history[0])));
+	moveRightWheels(powerRight/log10(abs(history[1])));
+}
+else {
+	wheelRecordLeft[tmpIndex]=powerLeft;
+	wheelRecordRight[tmpIndex]=powerRight;
+	moveLeftWheels(powerLeft);
+	moveRightWheels(powerRight);
+	return;
+}
+}
+
+}
+
 
 void mobileGoalUp()
 {
@@ -47,29 +115,55 @@ void mobileGoalStop(){
 
 void fourBarUp()
 {
-	motor[fb_topleft] = FOURBAR_POWER;
-	motor[fb_bottomleft] = FOURBAR_POWER;
-	motor[fb_topright] = FOURBAR_POWER;
-	motor[fb_bottomright] = FOURBAR_POWER;
+	motor[fb_left] = FOURBAR_POWER+FOURBAR_ANTIGRAVITY;
+	motor[fb_right] = FOURBAR_POWER+FOURBAR_ANTIGRAVITY;
 }
 
 void fourBarDown()
 {
 
-	motor[fb_topleft] = -FOURBAR_POWER;
-	motor[fb_bottomleft] = -FOURBAR_POWER;
-	motor[fb_topright] = -FOURBAR_POWER;
-	motor[fb_bottomright] = -FOURBAR_POWER;
+	motor[fb_left] = -FOURBAR_POWER+FOURBAR_ANTIGRAVITY;
+	motor[fb_right] = -FOURBAR_POWER+FOURBAR_ANTIGRAVITY;
+
 }
 
 void fourBarStop()
 {
-
-	motor[fb_topleft] = FOURBAR_ANTIGRAVITY;
-	motor[fb_bottomleft] = FOURBAR_ANTIGRAVITY;
-	motor[fb_topright] = FOURBAR_ANTIGRAVITY;
-	motor[fb_bottomright] = FOURBAR_ANTIGRAVITY;
+	motor[fb_left] = FOURBAR_ANTIGRAVITY;
+	motor[fb_right] = FOURBAR_ANTIGRAVITY;
 }
+
+void rollerIn(){
+	motor[roller]=ROLLER_POWER;
+}
+
+void rollerOut(){
+	motor[roller]=-ROLLER_POWER;
+}
+
+void rollerStop(){
+	motor[roller]=ROLLER_ANTIGRAVITY;
+}
+
+void rollerZero(){
+	motor[roller]=0;
+}
+
+void chainBarUp(){
+	motor[chainbar]=CHAINBAR_POWER+CHAINBAR_ANTIGRAVITY;
+
+}
+
+void chainBarDown(){
+	motor[chainbar]=-CHAINBAR_POWER+CHAINBAR_ANTIGRAVITY;
+}
+
+void chainBarStop(){
+	motor[chainbar]=CHAINBAR_ANTIGRAVITY;
+}
+
+
+
 
 //----------------END Utility Functions------------
 
@@ -78,12 +172,13 @@ task WheelControls()
 	for (;;) // a foreverloop
 	{
 		moveLeftWheels(AxisLeftWheels);
-		moveRightWheels(
-		AxisRightWheels);
+		moveRightWheels(AxisRightWheels);
+		//bufferedWheelsControls(AxisLeftWheels,AxisRightWheels);
 	}
 }
 
-task MobileGoalControls(){
+task MobileGoalControls()
+{
 	for (;;){
 		while (ButtonMobileGoalDown) mobileGoalDown();
 		while (ButtonMobileGoalUp) mobileGoalUp();
@@ -91,12 +186,42 @@ task MobileGoalControls(){
 	}
 }
 
-task FourBarControls(){
+task FourBarControls()
+{
 	for (;;){
 		while (ButtonFourbarUp) fourBarUp();
 		while (ButtonFourbarDown) fourBarDown();
 		fourBarStop();
 		}
+}
+
+task RollerControls()
+{
+	bool shouldStop=true;
+	for (;;){
+		if (ButtonRollerIn){
+			rollerIn();
+			shouldStop=false;
+		}
+		else if (ButtonRollerOut){
+			rollerOut();
+			shouldStop=true;
+		}
+		else
+		{
+			if (shouldStop) rollerZero();
+			else rollerStop();
+
+}
+}}
+
+task ChainBarControls()
+{
+	for (;;){
+		while (ButtonChainBarUp) chainBarUp();
+		while (ButtonChainBarDown) chainBarDown();
+		chainBarStop();
+}
 }
 
 
--- UserControls.h
+++ UserControls.h
@@ -2,9 +2,13 @@
 #define userControls
 
 // ------------Driver Control Codes-------------
-	startTask (WheelControls);
-	startTask (MobileGoalControls);
-	startTask (FourBarControls);
+startTask (WheelControls);
+startTask (MobileGoalControls);
+startTask (FourBarControls);
+startTask (RollerControls);
+startTask (ChainBarControls);
+for(;;){}
+
 // ------------END Driver Control Main Codes-----------
 
 #endif
