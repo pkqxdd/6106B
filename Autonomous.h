7d7b12eba5fc3c1fb2bdb6316d635617113a4802
Ruocheng Wang <yrrejkk@gmail.com>
2018-01-12 23:47:22 -0500

    Updated Autonomous Untested

--- Autonomous.h
+++ Autonomous.h
@@ -1,29 +1,30 @@
 #ifndef Autonomous
 #define Autonomous
+
 startTask(SmoothMobileGoalLift);
 
-nMotorEncoder[front_left]=0;
-nMotorEncoder[front_right]=0;
-nMotorEncoder[back_left]=0;
-nMotorEncoder[back_right]=0;
+resetEncoders();
 
+// Initializing
 holdChainBar(1950);
 holdFourBar(675);
 moveToMobileGoalLevel(0);
 
-
+// Move to the mobile goal
 AbsoluteMoveAllWheelsTo(3000);
-wait1Msec(500);
+wait1Msec(1000); // wait to stop
+
+// lifting mobile goal while dropping the cone
 moveToMobileGoalLevel(3);
 rollerOut();
-wait1Msec(2000);
-AbsoluteMoveAllWheelsTo(0);
+wait1Msec(2000); // wait for mobile goal lift
 
-
-
-
+// go back to starting position
+AbsoluteMoveAllWheelsTo(0); 
 
 
 
 stopTask(SmoothMobileGoalLift);
+
+
 #endif
--- autonomousHelpers.h
+++ autonomousHelpers.h
@@ -2,46 +2,72 @@
 #define autonomousHelpers
 #include "Driver.h"
 
+void MoveAllWheelsStraight(const int power){
+#define fl nMotorEncoder[front_left]*FRONT_LEFT_DIRECTION
+#define bl nMotorEncoder[back_left]*BACK_LEFT_DIRECTION
+#define fr nMotorEncoder[front_right]*FRONT_RIGHT_DIRECTION
+#define br nMotorEncoder[back_right]*BACK_RIGHT_DIRECTION 
+
+#define scaleFactor 0.5
+
+int diffFront= scaleFactor * (fr-fl);
+int diffBack = scaleFactor * (br-bl);
+
+motor[front_left]	= power - diffFront;
+motor[front_right]	= power + diffFront;
+motor[back_left]  	= power - diffBack;
+motor[back_right] 	= power + diffBack;
+ 
+#undef front_left 
+#undef back_left 
+#undef front_right
+#undef back_right 
+}
 
 void AbsoluteMoveAllWheelsTo(int target){
-#define currLoc (abs(nMotorEncoder(front_left))+abs(nMotorEncoder(back_left))+abs(nMotorEncoder(front_right))+abs(nMotorEncoder(back_right)))/4
+//#define currLoc (abs(nMotorEncoder(front_left))+abs(nMotorEncoder(back_left))+abs(nMotorEncoder(front_right))+abs(nMotorEncoder(back_right)))/4
+#define currLoc (\
+	nMotorEncoder[front_left]*FRONT_LEFT_DIRECTION + \
+	nMotorEncoder[front_right]*FRONT_RIGHT_DIRECTION + \
+	nMotorEncoder[back_left]*BACK_LEFT_DIRECTION + \
+	nMotorEncoder[back_right]*BACK_RIGHT_DIRECTION ) / 4 //average
 
-
-int dis[4]={0,0,0,0}; //record for the distance of wheels
 
 if (currLoc<target){
-while (currLoc<target){ //if average of four wheels...
+while (currLoc<target){ // go forward
 
-	dis[0]=nMotorEncoder(front_left);
-	dis[1]=nMotorEncoder(front_right);
-	dis[2]=nMotorEncoder(back_left);
-	dis[3]=nMotorEncoder(back_right);
-	moveLeftWheels(100);
-	moveRightWheels(95);
+	MoveAllWheelsStraight(80);
+	// moveLeftWheels(100);
+	// moveRightWheels(95);
 
 }
 }
 else {
-	while (currLoc>target){ //if average of four wheels...
+	while (currLoc>target){ //go backward
 
-	dis[0]=nMotorEncoder[front_left];
-	dis[1]=nMotorEncoder[front_right];
-	dis[2]=nMotorEncoder[back_left];
-	dis[3]=nMotorEncoder[back_right];
-	moveLeftWheels(-100);
-	moveRightWheels(-95);
+	MoveAllWheelsStraight(-80);
+	// moveLeftWheels(-100);
+	// moveRightWheels(-95);
 }
 }
-moveLeftWheels(0);
+
+moveLeftWheels(0); //stop
 moveRightWheels(0);
-
-}
 #undef currLoc
+}
+
 
 
 void moveToMobileGoalLevel(const int level){
 
 mb_level=level;
+}
+
+void resetEncoders(){
+nMotorEncoder[front_left]=0;
+nMotorEncoder[front_right]=0;
+nMotorEncoder[back_left]=0;
+nMotorEncoder[back_right]=0;
 }
 
 
