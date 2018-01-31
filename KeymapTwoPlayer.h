b4ce6ca79bf6590d7f21d1465f10ac3ddb89445e
pkqxdd <pkqxdd@users.noreply.github.com>
2018-01-31 09:25:18 -0500

    Rebuild Continued

--- KeymapTwoPlayer.h
+++ KeymapTwoPlayer.h
@@ -6,21 +6,21 @@
 #define ButtonChainBarUp vexRT[Btn7U]
 #define ButtonChainBarDown vexRT[Btn7L]
 #define ButtonSpecialPickUp vexRT[Btn7R]
-#define ButtonSpecialDropOff vexRT[Btn7D]
+#define ButtonSpecialDropOffAdapt vexRT[Btn7D]
 
 
 #define ButtonRollerIn vexRT[Btn8U]
 #define ButtonRollerOut vexRT[Btn8R]
-#define ButtonSpecialDropOffLow vexRT[Btn8D]
-#define ButtonSpecialDropOffHigh vexRT[Btn8L]
 
 #define AxisLeftWheels vexRT[Ch3]
 #define AxisRightWheels vexRT[Ch2]
 
+#define ButtonMobileGoalUp vexRT[Btn5U]
+#define ButtonMobileGoalDown vexRT[Btn5D]
+
+
 //----------------Second Player-------------
 
-#define ButtonMobileGoalUp vexRT[Btn5UXmtr2]
-#define ButtonMobileGoalDown vexRT[Btn5DXmtr2]
 
 #define ButtonConeCountIncrement vexRT[Btn8RXmtr2]
 #define ButtonConeCountDecrement vexRT[Btn8DXmtr2]
