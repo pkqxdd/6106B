80fbcb8baff7e5f848d153a33da1b62888231794
Jack Fenton <mllax8@me.com>
2018-02-18 22:43:02 -0500

    possible new 10pt autonon

--- autonomousTurnLeft10.h
+++ autonomousTurnLeft10.h
@@ -1,31 +1,33 @@
 motor[roller]=ROLLER_ANTIGRAVITY;
 
 
-holdFourBar(150);
+//holdFourBar(150);
 mb_out(false);
 
-//turn(-47.5,5);
+holdFourBar(1030);
+holdChainBar(4095);
+
 wait1Msec(400);
-moveWheels(49,true,1.25);
-
-wait1Msec(500);
-
-//mobileGoal(940,true);
-
-
+moveWheels(49,true,2.2);
+wait1Msec(200);
 mb_in(true);
-turn(179,5);
 
-wait1Msec(300);
-holdFourBar(0);
-holdChainBar(400);
-
-moveWheels(40,true,1.5);
-//motor[roller]=-ROLLER_POWER;
-
+holdFourBar(630);
 wait1Msec(300);
 
+motor[roller]=-ROLLER_POWER;
+wait1Msec(200);
+holdFourBar(900);
 
+moveWheels(-44,true,1.5);
+
+motor[roller]=0;
+
+turn(180);
+wait1Msec(100);
+
+moveWheels(10,true,1.5);
+//probably need more than this
 mb_out(true);
 
-moveWheels(-10,true,2);
+moveWheels(-50, true, 1)
