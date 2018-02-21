cc2013a83ab1078887a993c34c2acdb525a2230e
Jerry Wang <pkqxdd@users.noreply.github.com>
2018-02-20 22:55:50 -0500

    skillChallenge Solution in Theory

--- skillChallenge.h
+++ skillChallenge.h
@@ -48,14 +48,14 @@ wait1Msec(500);
 mb_out(false);
 moveWheels(-18,true,2);
 
-//finish first mobile goal
+//finish first mobile goal (22 points)
 
 mb_mid(false);
 wait1Msec(400);
 turn(90);
 wait1Msec(300);
 
-moveWheels(14,true,2); // in 5 point zone
+moveWheels(14,true,2); // move in 5 point zone
 
 mb_out(false);
 turn(90);
@@ -74,7 +74,7 @@ moveWheels(22,true,3);
 wait1Msec(600);
 mobileGoal(1800,false);
 
-//finish second mobile goal
+//finish second mobile goal (32 points)
 
 turn(-180);
 wait1Msec(600);
@@ -84,38 +84,81 @@ moveWheels(42*sqrt(2)-4,true);
 mb_in(true); // take the mobile goal
 mb_mid(false);
 moveWheels(36*sqrt(2)-2,true,2);
-stopAllTasks();
 
-//finish third mobile goal
-mb_in(false);
-turn(-175);
-moveWheels(-2,true,1);
+//finish third mobile goal (42 points)
 
-clearTimer(T1);
-releaseWheels();
-while (time1(T1)<1300){
-moveLeftWheels(-35);
-moveRightWheels(-35);
-}
+moveWheels(-3,true);
+turn(225);
 mb_out(false);
-moveWheels(60,true,2);
-mb_mid(true); // take the mobile goal
-moveWheels(30,true,2);
-mb_out(true);
-moveWheels(-10,true,2);
+wait1Msec(400);
 
-// finish fourth mobile goal (first one on the opposite side)
+moveWheels(58,true,2);
+mb_in(true);
+moveWheels(49,true,2.2);
+wait1Msec(500);
+mb_in(true);
+
+wait1Msec(300);
+
+moveWheels(-41,true,1.5);
+wait1Msec(500);
+
+turn(-135);
+
+holdChainBar(600);
+
+wait1Msec(500);
+holdFourBar(400);
+
+moveWheels(21.5,true,2.5);
+wait1Msec(700);
+
+turn(-90);
+wait1Msec(300);
+
+mobileGoal(2270,false,50);
+moveWheels(22,true,4);
+mb_mid(false);
+
+wait1Msec(500);
+
+
+mb_out(false);
+moveWheels(-18,true,2);
+
+// finish fourth mobile goal (62 points)
 
 mb_mid(false);
-moveWheels(-29,true);
+wait1Msec(400);
+turn(-90);
+wait1Msec(300);
+
+moveWheels(14,true,2); // move in 5 point zone
+
 mb_out(false);
-turn(-80);
-moveWheels(35,true,2);
+turn(-90);
+
+wait1Msec(300);
+
+moveWheels(24,false,2); // go to mobile goal
+wait1Msec(300);
+mb_in(true);  // take the mobile goal
+wait1Msec(300);
+
+turn(180);
+wait1Msec(600);
+mb_mid(false);
+moveWheels(22,true,3);
+wait1Msec(600);
+mobileGoal(1800,false);
+
+//finish fifth mobile goal (72 points)
+turn(-180);
+wait1Msec(600);
+mb_out(false);
+moveWheels(42*sqrt(2)-4,true);
+
 mb_in(true); // take the mobile goal
-turn(80);
 mb_mid(false);
-moveWheels(47,true,2);
-mb_out(true);
-moveWheels(-5,true,2);
-
-//finish fifth mobile goal
+moveWheels(36*sqrt(2)-2,true,2);
+//finish sixth mobile goal (82 points)
