1f53226f68610e1e25cfb617b6a97a438e7dcc17
Ruocheng Wang <yrrejkk@gmail.com>
2018-02-23 21:22:23 -0500

    Remap keys

--- KeymapTwoPlayer.h
+++ KeymapTwoPlayer.h
@@ -2,17 +2,6 @@
 #define keymap
 //--------------First Player--------------
 
-//#define ButtonFourbarUp vexRT[Btn6U]
-//#define ButtonFourbarDown vexRT[Btn6D]
-
-//#define ButtonChainBarUp vexRT[Btn7U]
-//#define ButtonChainBarDown vexRT[Btn7L]
-//#define ButtonSpecialPickUp vexRT[Btn7R]
-//#define ButtonSpecialDropOffAdapt vexRT[Btn7D]
-
-
-//#define ButtonRollerIn vexRT[Btn8U]
-//#define ButtonRollerOut vexRT[Btn8R]
 
 #define AxisLeftWheels vexRT[Ch3]
 #define AxisRightWheels vexRT[Ch2]
@@ -20,8 +9,6 @@
 #define ButtonMobileGoalUp vexRT[Btn5U]
 #define ButtonMobileGoalDown vexRT[Btn5D]
 
-//#define ButtonChainBarOverrideStart vexRT[Btn8D]
-//#define ButtonChainBarOverrideStop vexRT[Btn8L]
 #define ButtonChainBarUp vexRT[Btn7UXmtr2]
 #define ButtonChainBarDown vexRT[Btn7LXmtr2]
 #define ButtonSpecialPickUp vexRT[Btn7R]
@@ -35,22 +22,12 @@
 //#define ButtonConeCountIncrement vexRT[Btn8RXmtr2]
 //#define ButtonConeCountDecrement vexRT[Btn8DXmtr2]
 //#define ButtonConeCountReset vexRT[Btn8LXmtr2]
-//#define ButtonChainBarOverrideUp vexRT[Btn6UXmtr2]
-//#define ButtonChainBarOverrideDown vexRT[Btn6DXmtr2]
 
 #define ButtonFourbarUp vexRT[Btn6UXmtr2]
 #define ButtonFourbarDown vexRT[Btn6DXmtr2]
-/*
-#define ButtonChainBarUp vexRT[Btn7UXmtr2]
-#define ButtonChainBarDown vexRT[Btn7LXmtr2]
-//#define ButtonSpecialPickUp vexRT[Btn7RXmtr2]
-#define ButtonSpecialDropOffAdapt vexRT[Btn7DXmtr2]
-*/
 
 #define ButtonRollerIn vexRT[Btn8UXmtr2]
 #define ButtonRollerOut vexRT[Btn8RXmtr2]
-#define ButtonChainBarOverrideStart vexRT[Btn8DXmtr2]
-#define ButtonChainBarOverrideStop vexRT[Btn8LXmtr2]
 
 
 #endif
