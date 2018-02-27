9538ff2dbd4d80b3d6435287b7a0f5bfbbb2d27d
Wang20 <jwang20@choate.edu>
2018-02-27 18:42:06 -0500

    Continuous Generic Integration

--- skillChallenge.h
+++ skillChallenge.h
@@ -35,11 +35,11 @@ turn(135);
 moveWheels(22.5,true,2.5);
 wait1Msec(200);
 
-turn(90);
+turn(93);
 wait1Msec(200);
 
 mobileGoal(2070,false,50,200);
-moveWheels(26,true,5);
+moveWheels(26,true,4);
 //mb_mid(false);
 mobileGoal(2500);
 wait1Msec(500);
@@ -55,7 +55,7 @@ wait1Msec(600);
 turn(90);
 wait1Msec(300);
 
-moveWheels(12,true,2); // move in 5 point zone
+moveWheels(16,true,2); // move in 5 point zone
 wait1Msec(400);
 mb_out(false);
 turn(90);
@@ -70,27 +70,28 @@ wait1Msec(300);
 turn(-180);
 wait1Msec(600);
 mb_mid(false);
-moveWheels(24,true,3);
-wait1Msec(600);
+moveWheels(30,true,1.5);
 mobileGoal(1800,false);
-
+wait1Msec(600);
 //finish second mobile goal (32 points)
+moveWheels(-5,true,1);
+wait1Msec(200);
 
-
-
-turn(-180);
+turn(180);
 wait1Msec(600);
 mb_out(false);
 moveWheels(42*sqrt(2)-4,true,2);
 
-mb_in(true); // take the mobile goal
+//mb_in(true); // take the mobile goal
 mb_mid(false);
 moveWheels(36*sqrt(2)-2,true,2);
-
+mobileGoal(1800,false);
 //finish third mobile goal (42 points)
 
-moveWheels(-3,true);
-turn(235);
+moveWheels(-3,true,1);
+wait1Msec(200);
+turn(-135);
+
 mb_out(false);
 wait1Msec(600);
 
@@ -123,40 +124,43 @@ wait1Msec(500);
 
 mb_out(false);
 moveWheels(-18,true,2);
-
 // finish fourth mobile goal (62 points)
 
+
 mb_mid(false);
-wait1Msec(400);
+wait1Msec(600);
 turn(90);
 wait1Msec(300);
 
-moveWheels(14,true,2); // move in 5 point zone
-
+moveWheels(16,true,2); // move in 5 point zone
+wait1Msec(400);
 mb_out(false);
-turn(-90);
+turn(90);
 
-wait1Msec(300);
+wait1Msec(400);
 
 moveWheels(24,false,2); // go to mobile goal
 wait1Msec(300);
 mb_in(true);  // take the mobile goal
 wait1Msec(300);
 
-turn(180);
-wait1Msec(600);
-mb_mid(false);
-moveWheels(22,true,3);
-wait1Msec(400);
-mobileGoal(1800,false);
-
-//finish fifth mobile goal (72 points)
 turn(-180);
 wait1Msec(600);
-mb_out(false);
-moveWheels(42*sqrt(2)-4,true);
+mb_mid(false);
+moveWheels(30,true,1.5);
+mobileGoal(1800,false);
+wait1Msec(600);
+//finish fifth mobile goal (72 points)
+moveWheels(-5,true,1);
+wait1Msec(200);
 
-mb_in(true); // take the mobile goal
+turn(180);
+wait1Msec(600);
+mb_out(false);
+moveWheels(42*sqrt(2)-4,true,2);
+
+//mb_in(true); // take the mobile goal
 mb_mid(false);
 moveWheels(36*sqrt(2)-2,true,2);
+mobileGoal(1800,false);
 //finish sixth mobile goal (82 points)
